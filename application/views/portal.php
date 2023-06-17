c743af63c96bbaadb06941b28d95f2e3662a6231
Mnurfauzi <muhammadnurfauzi12@gmail.com>
2023-06-17 18:29:37 +0700

    baru

---
+++ application/views/portal.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>SPP | SDIT INSAN MADANI BEKASI</title>
+	<link rel="icon" type="image/png" href="<?php echo media_url('img/icon.png') ?>">
+
+	<!-- Bootstrap Core CSS -->
+	<link href="<?php echo media_url() ?>/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo media_url() ?>/css/load-font-googleapis.css">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="<?php echo media_url() ?>/css/font-awesome.min.css">
+
+
+	<!-- Custom CSS -->
+	<link href="<?php echo media_url() ?>/css/frontend-style.css" rel="stylesheet">
+	<link href="<?php echo media_url() ?>/css/portal.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+	<!-- Home -->
+<body style="background-image:url(asset/images/icon/Header_logo.png);background-size:cover;">
+    <div class="page-wrapper">
+        <div class="page-content--bge5">
+            <div class="container">
+                <div class="login-wrap">
+                    <div class="login-content">
+	<section class="content-section">
+		<div class="container text-center">
+			<div class="row">
+				<div class="col-md-12">
+					<h1><i class="fa fa-graduation-cap"></i> SELAMAT DATANG <i class="fa fa-graduation-cap"></i> 
+					<p class="lead mb-5 colr">SISTEM PEMBAYARAN SPP SDIT INSAN MADANI KOTA BEKASI</p>
+				</div>	
+				<div class="col-md-4">
+					<a href="<?php echo site_url('manage') ?>">
+					<div class="box">
+						<i class="fa fa-desktop icon-menu"></i>
+						<br>
+						Login Admin
+					</div>
+				</a>
+				</div>
+				<div class="col-md-4">
+					<a href="<?php echo site_url('home') ?>">
+					<div class="box">
+						<i class="fa fa-credit-card icon-menu"></i>
+						<br>
+						Cek Pembayaran Siswa
+					</div>
+				</a>
+				</div>
+				<div class="col-md-4">
+					<a href="<?php echo site_url('student') ?>">
+					<div class="box">
+						<i class="fa fa-users icon-menu"></i>
+						<br>
+						Login Siswa
+					</div>
+				</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+</body>
+
+</html>
